ced8d11c404c68d2d148f37617d4bf98a9438144
amartinezg <a.martinezgarcia@edu.gva.es>
2025-03-27 19:32:53 +0100

    hoy

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,19 +1,68 @@
 package com.example.calculadora;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import model.Operador;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView display;
+
+    private Button buttonClear, buttonPlus, buttonEquals;
+    private Operador operador;
+    private Double operando;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+        buttonClear = findViewById(R.id.buttonClear);
+        buttonEquals = findViewById(R.id.buttonEquals);
+        buttonPlus = findViewById(R.id.buttonPlus);
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText("0");
+            }
+        });
+
+        buttonPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                operando = Double.parseDouble(display.getText().toString());
+                operador = Operador.SUMA;
+                display.setText("0");
+            }
+        });
+
+        buttonEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Double operando2 = Double.parseDouble(display.getText().toString());
+                Double resultado = 0.0;
+                if(operador == Operador.SUMA)
+                    resultado = operando + operando2;
+                display.setText(resultado.toString());
+            }
+        });
     }
+
+    public void onClick(View view){
+        if(display.getText().equals("0"))
+            display.setText(((Button) view).getText());
+        else
+            display.setText(display.getText().toString() + ((Button) view).getText());
+    }
+
 }
---
+++ app/src/main/java/model/Operador.java
@@ -0,0 +1,7 @@
+package model;
+
+public enum Operador {
+
+    SUMA, RESTA, MULTIPLICACION, DIVISION;
+
+}
